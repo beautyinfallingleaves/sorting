04c702fade4f0c3135ce56e4cab52e6bc4a0a0e6
Chris Kang <christopha.kang@gmail.com>
2019-10-23 11:33:00 -0500

    some test cases working

--- bubblesort.js
+++ bubblesort.js
@@ -9,6 +9,7 @@ function bubblesort(arr) {
  swapped = false;
  for (let i = 0; i < end; i++) {
    if (arr[i] > arr[i + 1]) {
+
     let results = swap(arr[i], arr[i+1]);
     arr[i] = results[0];
     arr[i+1] = results[1];
@@ -24,7 +25,6 @@ function bubblesort(arr) {
 
 function swap(left, right) {
   if (left < right) swapped = false;
-
   let temp = left;
   left = right;
   right = temp;
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,14 +1,10 @@
 describe("bubblesort", () => {
   beforeAll(() => {
-    spyOn('swap').callThrough();
+    spyOn(window, 'swap').and.callThrough();
   })
+
   it("returns an empty array if passed an empty array", () => {
     expect(bubblesort([])).toEqual([])
-  });
-
-  it("returns the sorted array", () => {
-    let unsorted = [-2, -3, 69, 420, 0];
-    expect(bubblesort(unsorted)).toEqual([-3, -2, 0, 69, 420]);
   });
 
   it("performs as many swaps as needed to sort the array", () => {
@@ -16,4 +12,10 @@ describe("bubblesort", () => {
     bubblesort(unsorted);
     expect(swap.calls.count()).toEqual(3);
   })
+
+  it("returns the sorted array", () => {
+    let unsorted = [-2, -3, 69, 420, 0];
+    expect(bubblesort(unsorted)).toEqual([-3, -2, 0, 69, 420]);
+  });
+
 });
